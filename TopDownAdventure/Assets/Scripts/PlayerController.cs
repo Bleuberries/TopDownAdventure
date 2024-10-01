f88f8f29537ebb69c6d5707efe4d7d5ffbcdc547
Mina <mylastnameiskumai@gmail.com>
2024-10-01 11:37:10 -0400

    Updated PlayerControllrer.cs

--- TopDownAdventure/Assets/Scripts/PlayerController.cs
+++ TopDownAdventure/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //importing SceneManagement.Library , always make sure to add when creating new scenes
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,7 +10,10 @@ public class PlayerController : MonoBehaviour
 
     //Sprite Variables 
     public Sprite upSprite;
-    public 
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite frontSprite;
+    
     //public Rigidbody2D rb;
     
     // Start is called before the first frame update
@@ -29,29 +33,41 @@ public class PlayerController : MonoBehaviour
         {
             newPosition.y += speed;
             //change sprite to up sprite
-            //sr.sprite = upSprite;
+            sr.sprite = upSprite;
         }
 
         //go left 
         if(Input.GetKey("a")) 
         {
             newPosition.x -= speed;
+            sr.sprite = leftSprite;
         }
 
         //go down
         if(Input.GetKey("s")) 
         {
             newPosition.y -= speed;
+            sr.sprite = frontSprite;
         }
 
         //go right
         if(Input.GetKey("d")) 
         { 
             newPosition.x += speed;
+            sr.sprite = rightSprite;
         }
         transform.position = newPosition;
     } 
-    
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        //check if colliding with a game object with specific tag 
+        if(collision.gameObject.tag.Equals("door1")) 
+        {
+            Debug.Log("change scene")
+            SceneManager.LoadScene(1);
+        }
+    }    
 }
 
 
